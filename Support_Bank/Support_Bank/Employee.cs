4549d44874ae1bda4d8dcefc559cec2671c1a970
Paul Hammersley <P.Hammersley@ucas.ac.uk>
2019-11-20 16:25:52 +0000

    Update - Tidied up Functionality

---
+++ Support_Bank/Support_Bank/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Support_Bank
+{
+    class Employee
+    {
+        public string NameFrom { get; set; }
+
+        public string NameTo { get; set; }
+
+        public string Balance { get; set; }   
+    }
+}
--- Support_Bank/Support_Bank/GetDatanadOrganise.cs
+++ Support_Bank/Support_Bank/GetDatanadOrganise.cs
@@ -8,47 +8,16 @@ namespace Support_Bank
         {
             string[] CSVFileLoad = System.IO.File.ReadAllLines(@"C:\Work\Training\Support_Bank\Support_Bank\Support_Bank\Transactions2014.csv");
             int ArrayLength = CSVFileLoad.Length;
-
-            
-            string userInput;
-            Console.Write("List All or List [Name] - ");
-            userInput = Console.ReadLine();
-            Console.WriteLine("You entered '{0}'", userInput);
-            
-
             for (int i = 1; i < ArrayLength; i++)
             {
                 string[] TransactionElement = CSVFileLoad[i].Split(',');
-                //Console.WriteLine(TransactionElement[2]);
-                //Array.ForEach(CSVFileLoad, Console.WriteLine);
-                //Console.ReadLine();
 
-                var dateObject = new GetDate();
-                var returnValueDate = dateObject.Date(TransactionElement);
+                Employee newEmployee = new Employee();
+                newEmployee.NameFrom = TransactionElement[1];
+                newEmployee.NameTo = TransactionElement[2];
+                newEmployee.Balance = TransactionElement[4];
 
-                var nameObject = new GetName();
-                var returnValueName = nameObject.Name(TransactionElement);
-
-                var owesObject = new GetOwesTo();
-                var returnValueOwes = owesObject.Owes(TransactionElement);
-
-                var reasonObject = new GetReason();
-                var returnValueReason = reasonObject.Reason(TransactionElement);
-
-                var amountObject = new GetAmount();
-                var returnValueAmount = amountObject.Amount(TransactionElement);
-
-                //if (Program.Main.userInput = "List All")
-                    //{
-                    //Console.WriteLine("test");
-                    //}
-                //else if (Program.Main.userInput = "List { }");
-                    //{
-                    //Console.WriteLine("test2");
-                    //}
-                
-
-                Console.WriteLine(returnValueDate + " " + returnValueName + " " + returnValueOwes + " " + returnValueReason + " £" + returnValueAmount);               
+                Console.WriteLine($"{newEmployee.NameFrom} owes {newEmployee.NameTo}, {newEmployee.Balance}");
             }
             Console.ReadLine();
             }
